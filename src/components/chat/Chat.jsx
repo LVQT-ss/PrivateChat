940f324be6c7d9c6cefa13e5493949dcd19e8450
LVQT <levietquocthinh@gmail.com>
2025-05-02 15:21:21 +0700

    chatList

---
+++ src/components/chat/Chat.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./chat.css";
+const chat = () => {
+  return <div className="chat">chat</div>;
+};
+
+export default chat;
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./detail.css";
+const detail = () => {
+  return <div className="detail">detial</div>;
+};
+
+export default detail;
