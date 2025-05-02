8fa5512973c5eda14b3ad888eb7879719e58f8e6
LVQT <levietquocthinh@gmail.com>
2025-05-02 16:48:49 +0700

    Chat session  and emoji picker

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.css";
-const chat = () => {
-  return <div className="chat">chat</div>;
+import EmojiPicker from "emoji-picker-react";
+
+const Chat = () => {
+  const [open, setOpen] = useState(false);
+  const [text, setText] = useState("");
+
+  const handleEmoji = (e) => {
+    setText((prevText) => prevText + e.emoji);
+    setOpen(false);
+  };
+
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>Jane Doe</span>
+            <p>Lorem ipsum dolor, sit amet.</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="center">
+        <div className="message ">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              reiciendis repudiandae aspernatur ab architecto.
+            </p>
+            <span> 1 min ago</span>
+          </div>
+        </div>
+        <div className="message ">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              reiciendis repudiandae aspernatur ab architecto.
+            </p>
+            <span> 1 min ago</span>
+          </div>
+        </div>
+        <div className="message own">
+          <div className="texts">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1z3WO2y5h7YkHljxIsvwuOxP21OE_8tnedA&s" />
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
+              reiciendis repudiandae aspernatur ab architecto.
+            </p>
+            <span> 1 min ago</span>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="icons ">
+          <img src="./img.png" alt="" />
+          <img src="./camera.png" alt="" />
+          <img src="./mic.png" alt="" />
+        </div>
+        <input
+          placeholder="type a message"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <div className="emoji">
+          <img
+            src="./emoji.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+          <div className="picker">
+            {open && <EmojiPicker onEmojiClick={handleEmoji} />}
+          </div>
+        </div>
+
+        <button className="sendButton">Send</button>
+      </div>
+    </div>
+  );
 };
 
-export default chat;
+export default Chat;
